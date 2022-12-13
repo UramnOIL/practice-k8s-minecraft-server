5accc4db2679bd48599d0330038dff98f669c2a4
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2022-12-14 00:58:09 +0900

    :tada: first commit

---
+++ main.tf
@@ -0,0 +1,237 @@
+locals {
+  ports = [
+    {
+      name = "minecraft"
+      nodePort =  30565
+      protocol = "TCP"
+      targetPort = 25565
+    }
+  ]
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+    config_context = "minikube"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+  config_context = "minikube"
+}
+
+resource "kubernetes_namespace" "istio-system" {
+  metadata {
+    name = "istio-system"
+    aannotations = [
+      "sidecar.jaegertracing.io/inject"
+    ]
+  }
+}
+
+# resource "kubernetes_namespace" "istio-operator" {
+#   metadata {
+#     name = "istio-operator"
+#   }
+# }
+
+# resource "helm_release" "istio-operator" {
+#   name = "istio-operator"
+#   repository = "https://wiremind.github.io/wiremind-helm-charts"
+#   chart = "istio-operator"
+#   namespace = "istio-system"
+# }
+
+resource "helm_release" "istio-base" {
+  name = "istio-base"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart = "base"
+  namespace = "istio-system"
+}
+
+resource "helm_release" "istiod" {
+  name = "istiod"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart = "istiod"
+  namespace = "istio-system"
+
+  depends_on = [helm_release.istio-base]
+
+  set {
+    name = "global.tracer.zipkin.address"
+    value = "jaeger-collector.istio-system.svc.cluster.local:14250"
+  }
+}
+
+resource "kubernetes_namespace" "istio-ingress" {
+  metadata {
+    labels = {
+      istio-injection = "enabled"
+    }
+
+    name = "istio-ingress"
+  }
+}
+
+resource "helm_release" "istio-ingress" {
+  name = "istio-ingressgateway"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart = "gateway"
+  namespace = "istio-ingress"
+
+  depends_on = [helm_release.istiod]
+  
+  set {
+    name = "service.type"
+    value = "NodePort"
+  }
+
+  set {
+    name = "service.ports[0].name"
+    value = "minecraft"
+  }
+
+  set {
+    name = "service.ports[0].port"
+    value = 25565
+  }
+
+  set {
+    name = "service.ports[0].protocol"
+    value = "TCP"
+  }
+
+  set {
+    name = "service.ports[0].targetPort"
+    value = 25565
+  }
+}
+
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+resource "helm_release" "prometheus" {
+  name = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart = "prometheus"
+  namespace = "monitoring"
+  create_namespace = true
+
+  depends_on = [helm_release.istiod]
+}
+
+resource "kubernetes_namespace" "kiali-operator" {
+  metadata {
+    name = "kiali-operator"
+  }
+}
+
+resource "helm_release" "kiali-operator" {
+  name = "kiali"
+  repository = "https://kiali.org/helm-charts"
+  chart = "kiali-operator"
+  namespace = "kiali-operator"
+
+  depends_on = [helm_release.prometheus]
+
+  set {
+    name = "cr.create"
+    value = "true"
+  }
+
+  set {
+    name = "cr.namespace"
+    value = "istio-system"
+  }
+
+  set {
+    name = "cr.spec.external_services.prometheus.url"
+    value = "http://prometheus-server.monitoring.svc.cluster.local"
+  }
+}
+
+# resource "kubernetes_manifest" "kiali" {
+#   depends_on = [helm_release.kiali-operator]
+#   manifest = {
+#     "apiVersion" = "kiali.io/v1alpha1"
+#     "kind" = "Kiali"
+#     "metadata" = {
+#       "name" = "kiali"
+#       "namespace"= "istio-system"
+#     }
+#     "spec" = {
+#       "auth" = {
+#         "strategy" = "token"
+#       }
+#       "deployment" = {
+#         "accessible_namespaces" = ["istio-ingress", "minecraft"]
+#         "view_only_mode" = "false"
+#       }
+#       "server" = {
+#         "web_root" = "/kiali"
+#       }
+#     }
+#   }
+# }
+
+resource "helm_release" "cert-manager" {
+  name = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart = "cert-manager"
+  namespace = "cert-manager"
+  version = "v1.10.1"
+  create_namespace = "true"
+
+  depends_on = [helm_release.istiod]
+
+  set {
+    name = "installCRDs"
+    value = "true"
+  }
+}
+
+resource "helm_release" "jaeger-operator" {
+  name = "jaeger-operator"
+  repository = "https://jaegertracing.github.io/helm-charts"
+  chart = "jaeger-operator"
+  namespace = "observability"
+
+  depends_on = [helm_release.istiod, helm_release.cert-manager]
+
+  set {
+    name = "jaeger.create"
+    value = "true"
+  }
+
+  set {
+    name = "jaeger.namespace"
+    value = "observability"
+  }
+
+  set {
+    name = "rbac.create"
+    value = "true"
+  }
+
+  set {
+    name = "serviceAccount.create"
+    value = "true"
+  }
+}
+
+# resource "kubernetes_manifest" "jaeger" {
+#   manifest = {
+#     "apiVersion" = "jaegertracing.io/v1"
+#     "kind": "Jaeger"
+#     "metadata" = {
+#       "name" = "allinone"
+#       "namespace" = "istio-system"
+#     }
+#   }
+
+#   depends_on = [helm_release.jaeger-operator]
+# }
