2eedcb87cab824c98dd4a444d1e29702a0d8b351
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2023-04-03 02:13:44 +0900

    :sparkles: provider requirements

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = "1.4.4"
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.9.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.19.0"
+    }
+  }
+}
+
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
